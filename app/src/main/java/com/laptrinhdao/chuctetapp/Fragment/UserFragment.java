b5ed356d734179c37b4e9320653b70482a77a5d4
TongNgocHoa <hyhy74159@gmail.com>
2016-12-10 12:55:15 +0700

    // new update

--- app/src/main/java/com/laptrinhdao/chuctetapp/Fragment/UserFragment.java
+++ app/src/main/java/com/laptrinhdao/chuctetapp/Fragment/UserFragment.java
@@ -15,7 +15,7 @@ public class UserFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.userfragmentt,container,false);
+        View view = inflater.inflate(R.layout.activity_dang_nhap,container,false);
         return view;
     }
 }
